74ae9ad6c78b83752544a2ddf3c2fa2d4f6f7e38
victorchu8 <victorchu1996@gmail.com>
2019-09-07 21:15:17 -0600

    Initial commit.

---
+++ Kibble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kibble
+//
+//  Created by VICTOR CHU on 2019-09-07.
+//  Copyright © 2019 Victor Chu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
