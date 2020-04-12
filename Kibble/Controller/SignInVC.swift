00b2736f192dcbb8f4f8ff04c9aee842566661d2
victorchu8 <victorchu1996@gmail.com>
2020-04-11 22:18:08 -0400

    Apple sign in boiler plate and app icons added

--- Kibble/Controller/SignInVC.swift
+++ Kibble/Controller/SignInVC.swift
@@ -7,14 +7,19 @@
 //
 
 import UIKit
+import AuthenticationServices
+import CryptoKit
 import FirebaseAuth
-import TransitionButton
+//import TransitionButton
 
+@available(iOS 13.0, *)
 class SignInVC: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var kibbleMainIcon: UIImageView!
     @IBOutlet weak var signInApple: UIButton!
     @IBOutlet weak var signInGoogle: UIButton!
+
+    fileprivate var currentNonce: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,6 +46,7 @@ class SignInVC: UIViewController, UITextFieldDelegate {
             signInApple.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
             signInApple.bottomAnchor.constraint(equalTo: signInGoogle.topAnchor, constant: -25.adjusted)
         ])
+        signInApple.addTarget(self, action: #selector(appleSignInTapped), for: .touchUpInside)
 
         signInGoogle.translatesAutoresizingMaskIntoConstraints = false
         signInGoogle.imageView?.contentMode = UIView.ContentMode.scaleAspectFit
@@ -50,7 +56,122 @@ class SignInVC: UIViewController, UITextFieldDelegate {
         ])
     }
 
+    @objc func appleSignInTapped() {
+        let nonce = randomNonceString()//Nonce.randomNonceString()
+        currentNonce = nonce
+        let provider = ASAuthorizationAppleIDProvider()
+        let request = provider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        request.nonce = sha256(nonce)
+
+        // pass the request to the initializer of the controller
+        let authController = ASAuthorizationController(authorizationRequests: [request])
+
+        // similar to delegate, this will ask the view controller
+        // which window to present the ASAuthorizationController
+        authController.presentationContextProvider = self
+
+        // delegate functions will be called when user data is
+        // successfully retrieved or error occured
+        authController.delegate = self
+
+        // show the Sign-in with Apple dialog
+        authController.performRequests()
+    }
+
+    func sha256(_ input: String) -> String {
+      let inputData = Data(input.utf8)
+      let hashedData = SHA256.hash(data: inputData)
+      let hashString = hashedData.compactMap {
+        return String(format: "%02x", $0)
+      }.joined()
+
+      return hashString
+    }
+
+    // Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
+    func randomNonceString(length: Int = 32) -> String {
+      precondition(length > 0)
+      let charset: Array<Character> =
+          Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+      var result = ""
+      var remainingLength = length
+
+      while remainingLength > 0 {
+        let randoms: [UInt8] = (0 ..< 16).map { _ in
+          var random: UInt8 = 0
+          let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+          if errorCode != errSecSuccess {
+            fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+          }
+          return random
+        }
+
+        randoms.forEach { random in
+          if remainingLength == 0 {
+            return
+          }
+
+          if random < charset.count {
+            result.append(charset[Int(random)])
+            remainingLength -= 1
+          }
+        }
+      }
+
+      return result
+    }
+
+}
+
+@available(iOS 13.0, *)
+extension SignInVC : ASAuthorizationControllerPresentationContextProviding, ASAuthorizationControllerDelegate{
+
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+           // return current view to put up authorization screen
+           return self.view.window!
+       }
+
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            guard let nonce = currentNonce else {
+                fatalError("Invalid state: A login callback was received, but no login request was sent.")
+            }
+            guard let appleIDToken = appleIDCredential.identityToken else {
+                print("Unable to fetch identity token")
+                return
+            }
+            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+                return
+            }
+            // Initialize a Firebase credential.
+            let credential = OAuthProvider.credential(withProviderID: "apple.com",
+                                                      idToken: idTokenString,
+                                                      accessToken: nonce)
+            // Sign in with Firebase.
+            Auth.auth().signIn(with: credential) { (authResult, error) in
+                if (error != nil) {
+                    // Error. If error.code == .MissingOrInvalidNonce, make sure
+                    // you're sending the SHA256-hashed nonce as a hex string with
+                    // your request to Apple.
+                    print(error?.localizedDescription)
+                    return
+                }
+                // User is signed in to Firebase with Apple.
+                // ...
+                print("********* SUCCESS **********")
+            }
+        }
+    }
+
+      func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        // Handle error.
+        print("Sign in with Apple errored: \(error)")
+      }
 
 
 }
+
+
 
